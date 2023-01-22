fe95d8c79ef78af8dd1750b36d680bece99a4da6
John Lai <lai.john.py@gmail.com>
2023-01-22 01:00:14 -0500

    Add pseudo code in js

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+// Init function to start the game on load
+
+//start challenge click
+
+//start timer
+
+//show one question from the array of objects
+
+//display 4 possible answer
+
+// check if user input is correct
+
+// show results - Correct or incorrect
+
+//if correct continue
+
+//if answer is incorrect minus timer
+
+//show next question
+
+//end game either when there is no time left or all the questions have been asked
+
+//stop game
+
+// Show final score
+
+//ask user to input initials
+
+//store user name and points in local storage
+
+//show high scores display board
+
+//compare all users score and display highest point up to the top
+
+//go back btn
+//clear high score function
